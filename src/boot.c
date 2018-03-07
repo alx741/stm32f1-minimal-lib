3d2c89f4f7aee58ca343fbe12f2cb9575c0e7a49
Daniel Campoverde <alx@sillybytes.net>
2018-03-07 09:59:03 -0500

    Initialize data in RAM during boot process

--- src/boot.c
+++ src/boot.c
@@ -1,9 +1,12 @@
 #include "boot.h"
 #include "f1.h"
+#include <string.h>
 #include <stdint.h>
 #include <stdbool.h>
 
-extern uint32_t _stack_base;
+/* Linker script symbols */
+extern uint32_t _stack_base, _data_start, _data_load_start, _data_size,
+       _bss_start, _bss_size, _data_end;
 
 __attribute__ ((section(".VECTOR_TABLE")))
 VECTOR_TABLE_t VECTOR_TABLE =
@@ -27,6 +30,18 @@ VECTOR_TABLE_t VECTOR_TABLE =
 
 void __attribute__((naked)) RESET_HANDLER(void)
 {
+    /* Initialize .data in RAM */
+	uint32_t *data_src = &_data_load_start;
+	uint32_t *data_dst = &_data_start;
+    size_t data_size = (size_t) &_data_size;
+    while (data_size--)
+    {
+        *data_dst++ = *data_src++;
+    }
+
+    /* Initialize .rodata in RAM */
+
+    /* User program entry point */
     main();
 }
 
