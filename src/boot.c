09224256c1c5703319aec073be700fda57c1b1e6
Daniel Campoverde <alx@sillybytes.net>
2018-03-07 10:03:52 -0500

    Initialize bss in RAM during boot process

--- src/boot.c
+++ src/boot.c
@@ -5,12 +5,14 @@
 #include <stdbool.h>
 
 /* Linker script symbols */
-extern uint32_t _stack_base, _data_start, _data_load_start, _data_size,
-       _bss_start, _bss_size, _data_end;
+extern uint32_t _stack_base;
+extern uint32_t _data_start, _data_load_start, _data_size;
+extern uint32_t _bss_start,  _bss_size;
 
-__attribute__ ((section(".VECTOR_TABLE")))
+__attribute__((section(".VECTOR_TABLE")))
 VECTOR_TABLE_t VECTOR_TABLE =
-    { (uint32_t) &_stack_base
+{
+    (uint32_t) &_stack_base
     , RESET_HANDLER
     , NMI_HANDLER
     , HARD_FAULT_HANDLER
@@ -26,20 +28,26 @@ VECTOR_TABLE_t VECTOR_TABLE =
     , 0
     , PENDSV_HANDLER
     , SYSTICK_HANDLER
-    };
+};
 
 void __attribute__((naked)) RESET_HANDLER(void)
 {
     /* Initialize .data in RAM */
-	uint32_t *data_src = &_data_load_start;
-	uint32_t *data_dst = &_data_start;
+    uint32_t *data_src = &_data_load_start;
+    uint32_t *data_dst = &_data_start;
     size_t data_size = (size_t) &_data_size;
     while (data_size--)
     {
         *data_dst++ = *data_src++;
     }
 
-    /* Initialize .rodata in RAM */
+    /* Initialize .bss in RAM */
+    uint32_t *bss_start = &_bss_start;
+    size_t bss_size = (size_t) &_bss_size;
+    while (bss_size--)
+    {
+        *bss_start++ = 0;
+    }
 
     /* User program entry point */
     main();
@@ -47,7 +55,7 @@ void __attribute__((naked)) RESET_HANDLER(void)
 
 void HARD_FAULT_HANDLER(void)
 {
-    while(1); // Halt
+    while (1); // Halt
 }
 
 void MEMORY_MANAGEMENT_FAULT_HANDLER(void)
