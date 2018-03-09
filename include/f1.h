67e749a579fa5b53e5ea2f63dae12d95de6b066b
Daniel Campoverde <alx@sillybytes.net>
2018-03-09 06:56:29 -0500

    Clean IRQ handler declarations

--- include/f1.h
+++ include/f1.h
@@ -5,74 +5,41 @@
  *   IRQ handlers
  * ----------------------------------------------
  */
-void __attribute__ ((weak)) WWDG_ISR(void);
-void __attribute__ ((weak)) PVD_ISR(void);
-void __attribute__ ((weak)) TAMPER_ISR(void);
-void __attribute__ ((weak)) RTC_ISR(void);
-void __attribute__ ((weak)) FLASH_ISR(void);
-void __attribute__ ((weak)) RCC_ISR(void);
-void __attribute__ ((weak)) EXTI0_ISR(void);
-void __attribute__ ((weak)) EXTI1_ISR(void);
-void __attribute__ ((weak)) EXTI2_ISR(void);
-void __attribute__ ((weak)) EXTI3_ISR(void);
-void __attribute__ ((weak)) EXTI4_ISR(void);
-void __attribute__ ((weak)) DMA1_CHANNEL1_ISR(void);
-void __attribute__ ((weak)) DMA1_CHANNEL2_ISR(void);
-void __attribute__ ((weak)) DMA1_CHANNEL3_ISR(void);
-void __attribute__ ((weak)) DMA1_CHANNEL4_ISR(void);
-void __attribute__ ((weak)) DMA1_CHANNEL5_ISR(void);
-void __attribute__ ((weak)) DMA1_CHANNEL6_ISR(void);
-void __attribute__ ((weak)) DMA1_CHANNEL7_ISR(void);
-void __attribute__ ((weak)) ADC1_2_ISR(void);
-void __attribute__ ((weak)) USB_HP_CAN_TX_ISR(void);
-void __attribute__ ((weak)) USB_LP_CAN_RX0_ISR(void);
-void __attribute__ ((weak)) CAN_RX1_ISR(void);
-void __attribute__ ((weak)) CAN_SCE_ISR(void);
-void __attribute__ ((weak)) EXTI9_5_ISR(void);
-void __attribute__ ((weak)) TIM1_BRK_ISR(void);
-void __attribute__ ((weak)) TIM1_UP_ISR(void);
-void __attribute__ ((weak)) TIM1_TRG_COM_ISR(void);
-void __attribute__ ((weak)) TIM1_CC_ISR(void);
-void __attribute__ ((weak)) TIM2_ISR(void);
-void __attribute__ ((weak)) TIM3_ISR(void);
-void __attribute__ ((weak)) TIM4_ISR(void);
-void __attribute__ ((weak)) I2C1_EV_ISR(void);
-void __attribute__ ((weak)) I2C1_ER_ISR(void);
-void __attribute__ ((weak)) I2C2_EV_ISR(void);
-void __attribute__ ((weak)) I2C2_ER_ISR(void);
-void __attribute__ ((weak)) SPI1_ISR(void);
-void __attribute__ ((weak)) SPI2_ISR(void);
-void __attribute__ ((weak)) USART1_ISR(void);
-void __attribute__ ((weak)) USART2_ISR(void);
-void __attribute__ ((weak)) USART3_ISR(void);
-void __attribute__ ((weak)) EXTI15_10_ISR(void);
-void __attribute__ ((weak)) RTCALARM_ISR(void);
-void __attribute__ ((weak)) USBWAKEUP_ISR(void);
-void __attribute__ ((weak)) TIM8_BRK_ISR(void);
-void __attribute__ ((weak)) TIM8_UP_ISR(void);
-void __attribute__ ((weak)) TIM8_TRG_COM_ISR(void);
-void __attribute__ ((weak)) TIM8_CC_ISR(void);
-void __attribute__ ((weak)) ADC3_ISR(void);
-void __attribute__ ((weak)) FSMC_ISR(void);
-void __attribute__ ((weak)) SDIO_ISR(void);
-void __attribute__ ((weak)) TIM5_ISR(void);
-void __attribute__ ((weak)) SPI3_ISR(void);
-void __attribute__ ((weak)) UART4_ISR(void);
-void __attribute__ ((weak)) UART5_ISR(void);
-void __attribute__ ((weak)) TIM6_ISR(void);
-void __attribute__ ((weak)) TIM7_ISR(void);
-void __attribute__ ((weak)) DMA2_CHANNEL1_ISR(void);
-void __attribute__ ((weak)) DMA2_CHANNEL2_ISR(void);
-void __attribute__ ((weak)) DMA2_CHANNEL3_ISR(void);
-void __attribute__ ((weak)) DMA2_CHANNEL4_5_ISR(void);
-void __attribute__ ((weak)) DMA2_CHANNEL5_ISR(void);
-void __attribute__ ((weak)) ETH_ISR(void);
-void __attribute__ ((weak)) ETH_WKUP_ISR(void);
-void __attribute__ ((weak)) CAN2_TX_ISR(void);
-void __attribute__ ((weak)) CAN2_RX0_ISR(void);
-void __attribute__ ((weak)) CAN2_RX1_ISR(void);
-void __attribute__ ((weak)) CAN2_SCE_ISR(void);
-void __attribute__ ((weak)) OTG_FS_ISR(void);
+#define _weak_  void __attribute__ ((weak))
+_weak_     WWDG_ISR(void),                     I2C2_ER_ISR(void);
+_weak_     PVD_ISR(void),                      SPI1_ISR(void);
+_weak_     TAMPER_ISR(void),                   SPI2_ISR(void);
+_weak_     RTC_ISR(void),                      USART1_ISR(void);
+_weak_     FLASH_ISR(void),                    USART2_ISR(void);
+_weak_     RCC_ISR(void),                      USART3_ISR(void);
+_weak_     EXTI0_ISR(void),                    EXTI15_10_ISR(void);
+_weak_     EXTI1_ISR(void),                    RTCALARM_ISR(void);
+_weak_     EXTI2_ISR(void),                    USBWAKEUP_ISR(void);
+_weak_     EXTI3_ISR(void),                    TIM8_BRK_ISR(void);
+_weak_     EXTI4_ISR(void),                    TIM8_UP_ISR(void);
+_weak_     DMA1_CHANNEL1_ISR(void),            TIM8_TRG_COM_ISR(void);
+_weak_     DMA1_CHANNEL2_ISR(void),            TIM8_CC_ISR(void);
+_weak_     DMA1_CHANNEL3_ISR(void),            ADC3_ISR(void);
+_weak_     DMA1_CHANNEL4_ISR(void),            FSMC_ISR(void);
+_weak_     DMA1_CHANNEL5_ISR(void),            SDIO_ISR(void);
+_weak_     DMA1_CHANNEL6_ISR(void),            TIM5_ISR(void);
+_weak_     DMA1_CHANNEL7_ISR(void),            SPI3_ISR(void);
+_weak_     ADC1_2_ISR(void),                   UART4_ISR(void);
+_weak_     USB_HP_CAN_TX_ISR(void),            UART5_ISR(void);
+_weak_     USB_LP_CAN_RX0_ISR(void),           TIM6_ISR(void);
+_weak_     CAN_RX1_ISR(void),                  TIM7_ISR(void);
+_weak_     CAN_SCE_ISR(void),                  DMA2_CHANNEL1_ISR(void);
+_weak_     EXTI9_5_ISR(void),                  DMA2_CHANNEL2_ISR(void);
+_weak_     TIM1_BRK_ISR(void),                 DMA2_CHANNEL3_ISR(void);
+_weak_     TIM1_UP_ISR(void),                  DMA2_CHANNEL4_5_ISR(void);
+_weak_     TIM1_TRG_COM_ISR(void),             DMA2_CHANNEL5_ISR(void);
+_weak_     TIM1_CC_ISR(void),                  ETH_ISR(void);
+_weak_     TIM2_ISR(void),                     ETH_WKUP_ISR(void);
+_weak_     TIM3_ISR(void),                     CAN2_TX_ISR(void);
+_weak_     TIM4_ISR(void),                     CAN2_RX0_ISR(void);
+_weak_     I2C1_EV_ISR(void),                  CAN2_RX1_ISR(void);
+_weak_     I2C1_ER_ISR(void),                  CAN2_SCE_ISR(void);
+_weak_     I2C2_EV_ISR(void),                  OTG_FS_ISR(void);
 
 
 /* ----------------------------------------------
@@ -111,6 +78,7 @@ void __attribute__ ((weak)) OTG_FS_ISR(void);
 #define _RCC             0x40021000
 #define _FLASH_INTERFACE 0x40022000
 #define _CRC             0x40023000
+
 
 /* --------------------------------------------------------------
  *   General purpose and alternate function I/O (GPIO and AFIOs)
