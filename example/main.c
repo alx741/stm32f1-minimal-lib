102fd42230ca35619e5aabea5c900615b08dc874
Daniel Campoverde <alx@sillybytes.net>
2018-03-09 08:56:26 -0500

    Port example to use RCC registers

--- example/main.c
+++ example/main.c
@@ -1,20 +1,12 @@
-#include "boot.h"
 #include "f1.h"
 #include <stdbool.h>
 
 void delay(void);
-
 int counter = 5;
 
 int main(void)
 {
-    uint32_t* rcc_apb2enr = 0x40021000 + 0x18;
-    uint32_t* gpio_crh    = 0x40011000 + 0x04;
-    uint32_t* gpio_odr    = 0x40011000 + 0x0C;
-    uint32_t* gpio_bsrr   = _PORTC + 0x10;
-
-    *rcc_apb2enr |= (1 << 4);
-
+    RCC_APB2ENR->IOPCEN = true;
     PORTC->MODE15 = 0b10;
     PORTC->CNF15 = 0b00;
 
