da419eaa8e0ef201e960d2f428d9db924a946b4c
Daniel Campoverde <alx@sillybytes.net>
2018-03-13 11:22:26 -0500

    MPU6050 example

--- example/main.c
+++ example/main.c
@@ -1,8 +1,11 @@
 #include <f1.h>
 #include <rcc.h>
 #include <usart.h>
+#include <i2c.h>
+#include <external/mpu6050.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 void delay(void);
 
@@ -18,21 +21,38 @@ int main(void)
     PORTC->CNF14 = CNF_IN_PULL_UP_DOWN;
 
     usart_init_72mhz_9600baud();
+    i2c_init_100khz();
 
-    int somechar;
+    delay(); // Wait mpu6050
+
+    // Wake mpu6050 up
+    mpu6050_wake_up();
+
+    /* int somechar; */
     while (true)
     {
-        somechar = getchar();
-        if (somechar == 'o')
-        {
-            PORTC->ODR15 = true;
-            printf("on\r\n");
-        }
-        else
-        {
-            PORTC->ODR15 = false;
-            printf("off\r\n");
-        }
+        delay();
+
+        ACCEL_t accel = mpu6050_read_accel();
+        printf("x = %f, y = %f, z = %f\r\n", accel.x, accel.y, accel.z);
+
+        /* GYRO_t gyro = mpu6050_read_gyro(); */
+        /* printf("x = %f, y = %f, z = %f\r\n", gyro.x, gyro.y, gyro.z); */
+
+        /* float temp = mpu6050_read_temp(); */
+        /* printf("temp = %f\r\n", temp); */
+
+        /* somechar = getchar(); */
+        /* if (somechar == 'o') */
+        /* { */
+        /*     PORTC->ODR15 = true; */
+        /*     printf("on\r\n"); */
+        /* } */
+        /* else */
+        /* { */
+        /*     PORTC->ODR15 = false; */
+        /*     printf("off\r\n"); */
+        /* } */
     }
 }
 
